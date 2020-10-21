91256c14682273b6d94ab720b52a63423f81fe09
Alex <alexmogwaiii@gmail.com>
2020-10-22 02:11:09 +0300

    final

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,14 +1,14 @@
 ! function (e) {
   "function" != typeof e.matches && (e.matches = e.msMatchesSelector || e.mozMatchesSelector || e.webkitMatchesSelector || function (e) {
     for (var t = this, o = (t.document || t.ownerDocument).querySelectorAll(e), n = 0; o[n] && o[n] !== t;) ++n;
-    return Boolean(o[n])
+    return Boolean(o[n]);
   }), "function" != typeof e.closest && (e.closest = function (e) {
     for (var t = this; t && 1 === t.nodeType;) {
       if (t.matches(e)) return t;
-      t = t.parentNode
+      t = t.parentNode;
     }
-    return null
-  })
+    return null;
+  });
 }(window.Element.prototype);
 
 
@@ -36,10 +36,10 @@ document.addEventListener('DOMContentLoaded', function () {
     overlay.classList.remove('active');
   });
 
-  $(document).on('click', function(e) {
-    if(e.target.id == 'overlay-modal') {
-      $('.modal').removeClass('active');
+  body.addEventListener('click', function (e) {
+    if (e.target.id == 'overlay-modal') {
       body.classList.remove('scroll-hidden');
+      document.querySelector('.modal').classList.remove('active');
       overlay.classList.remove('active');
     }
   });
